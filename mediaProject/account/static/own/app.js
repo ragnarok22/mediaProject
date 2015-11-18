425fbf8fd4f1b1c27230499d3661abf9885d4840
Reinier Hernández <rhernandeza@facinf.uho.edu.cu>
2015-11-18 14:30:27 -0500

    empleando ajax para mostar los usuarios que estan conectados en la pagina principal

--- mediaProject/account/static/own/app.js
+++ mediaProject/account/static/own/app.js
@@ -1,15 +1,21 @@
 $(document).ready(function () {
-    join_by_ajax("#terms", "#terms-content", "/terms");
-    join_by_ajax("#privacy", "#privacy-content", "/privacy");
-    join_by_ajax("#about", "#about-content", "/about");
-    join_by_ajax("#contact", "#contact-content", "/contact/us");
+    join_click_by_ajax("#terms", "#terms-content", "/terms");
+    join_click_by_ajax("#privacy", "#privacy-content", "/privacy");
+    join_click_by_ajax("#about", "#about-content", "/about");
+    join_click_by_ajax("#contact", "#contact-content", "/contact/us");
+
+    loader = $('#spinner-users-in');
+    loader.css('display', 'inline');
+    $('#users-in').load('/users/in', function () {
+        loader.css('display', 'none');
+    });
 });
 
 function print(sms){
     console.log(sms);
 }
 
-function join_by_ajax(to, from, context){
+function join_click_by_ajax(to, from, context){
     $(to).click(function (event) {
         event.preventDefault();
         loader = $('#spinner-' + to.substr(1));
