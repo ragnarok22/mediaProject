157cad09ac54bb3a82dac5a2b3d3dd4bf28780ee
Reinier Hernández <rhernandeza@facinf.uho.edu.cu>
2015-11-19 16:05:54 -0500

    mostrando la informacion al principio

--- mediaProject/account/static/own/app.js
+++ mediaProject/account/static/own/app.js
@@ -6,16 +6,25 @@ $(document).ready(function () {
 
     join_click_by_ajax("#profile", "#information", "/profile/1", "#spinner-information");
 
+    //mostrar los usuarios que se encuentran en linea
     loader = $('#spinner-users-in');
     loader.css('display', 'inline');
     $('#users-in').load('/users/in', function () {
         loader.css('display', 'none');
     });
 
+    //mostrar los administradores que se encuentran en linea
     loaderStaff = $('#spinner-users-staff-in');
     loaderStaff.css('display', 'inline');
     $('#users-staff-in').load('/users/staff/in', function () {
         loaderStaff.css('display', 'none');
+    });
+
+    //mostrar la informacion reciente
+    loaderInfo = $('#spinner-information');
+    loaderInfo.css('display', 'inline');
+    $('#information').load('/informations', function () {
+        loaderInfo.css('display', 'none');
     });
 });
 
