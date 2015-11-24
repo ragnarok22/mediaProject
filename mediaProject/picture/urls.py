401c9e6cc9b4da48db4fbdcb2c143efb86afa124
Reinier Hernández <rhernandeza@facinf.uho.edu.cu>
2015-11-23 21:41:33 -0500

    url y vista para mostrar los detalles de las fotos

--- mediaProject/picture/urls.py
+++ mediaProject/picture/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import pictures_list
+from .views import pictures_list, picture_details
 
 
 urlpatterns = [
     url(r'^list/$', pictures_list, name='pictures_list'),
+    url(r'^(?P<pk>[0-9]+)/$', picture_details, name='pictures_details'),
 
 ]
--- mediaProject/picture/views.py
+++ mediaProject/picture/views.py
@@ -6,3 +6,8 @@ def pictures_list(request):
     pictures = Photo.objects.all()
     comments = Comment.objects.all()
     return render(request, 'pictures.html', locals())
+
+
+def picture_details(request, pk):
+    picture = Photo.objects.get(pk=pk)
+    return render(request, 'picture_details.html', locals())
