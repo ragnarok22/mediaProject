9c4645fb8b94919044c120bde69915119375dd5c
Reinier Hernández <rhernandeza@facinf.uho.edu.cu>
2015-11-23 22:44:14 -0500

    url y view para mostrar los detalles de un grupo

--- mediaProject/group/urls.py
+++ mediaProject/group/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import group_list
+from .views import group_list, group_detail
 
 
 urlpatterns = [
     url(r'^list/$', group_list, name='group_list'),
+    url(r'^(?P<pk>[0-9]+)/$', group_detail, name='group_detail'),
 ]
--- mediaProject/group/views.py
+++ mediaProject/group/views.py
@@ -6,3 +6,9 @@ def group_list(request):
     groups = Group.objects.all()
     members = MemberShip.objects.all()
     return render(request, 'group_list.html', locals())
+
+
+def group_detail(request, pk):
+    group = Group.objects.get(pk=pk)
+    members = MemberShip.objects.filter(group_id=pk)
+    return render(request, 'group_detail.html', locals())
