2c65aeca5d6ba309603175e81a1b29087dc9368a
Reinier Hernández <rhernandeza@facinf.uho.edu.cu>
2015-11-23 20:53:37 -0500

    anadiendo la aplicacion picture y creando el modelo Photo

---
+++ mediaProject/picture/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+def url(self, filename):
+    route = 'pictures/%s/%s' % (self.owner, filename)
+    return route
+
+
+class Photo(models.Model):
+    owner = models.ForeignKey(User)
+    picture = models.ImageField(upload_to=url)
+    title = models.CharField(max_length=100)
+    about = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "%s del usuario %s" % (self.title, self.owner)
