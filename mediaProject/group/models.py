e31842407e71ad88173f305b75ccc500811ac924
Reinier Hernández <rhernandeza@facinf.uho.edu.cu>
2015-11-23 22:26:14 -0500

    añadiendo componentes a los grupos

--- mediaProject/group/models.py
+++ mediaProject/group/models.py
@@ -8,9 +8,15 @@ class Group(models.Model):
     owner = models.ForeignKey(User)
     members = models.ManyToManyField(User, through='MemberShip', through_fields=('group', 'person'), related_name='members_group')
 
+    def __str__(self):
+        return self.name
+
 
 class MemberShip(models.Model):
     group = models.ForeignKey(Group)
     person = models.ForeignKey(User)
     inviter = models.ForeignKey(User, related_name='membership_invites')
     invite_reason = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "%s --> %s" % (self.group, self.person)
