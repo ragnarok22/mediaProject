cc9e1adc9a981dded11f6b06e321b45403ad8de7
Reinier Hernández <rhernandeza@facinf.uho.edu.cu>
2015-11-23 22:49:19 -0500

    mostrando el listado de los grupos en el index con ajax

--- mediaProject/account/static/own/app.js
+++ mediaProject/account/static/own/app.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
     join_click_by_ajax("#contact", "#contact-content", "/contact/us");
 
     join_click_by_ajax("#images", "#information", "/pictures/list", "#spinner-information");
+    join_click_by_ajax("#groups", "#information", "/group/list", "#spinner-information");
 
     join_click_by_ajax("#init", "#information", "/informations", "#spinner-information");
 
