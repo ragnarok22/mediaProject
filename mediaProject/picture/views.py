64ec8648f60f5e348c14870c36fb85029f20eaaa
Reinier Hernández <rhernandeza@facinf.uho.edu.cu>
2015-11-23 21:35:42 -0500

    mostrando los comentarios de todas las fotos subidas

--- mediaProject/picture/views.py
+++ mediaProject/picture/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from .models import Photo
+from .models import Photo, Comment
 
 
 def pictures_list(request):
     pictures = Photo.objects.all()
+    comments = Comment.objects.all()
     return render(request, 'pictures.html', locals())
