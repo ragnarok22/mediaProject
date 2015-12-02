c3904380e4b05e38d53456fbc4529f410c3793de
Reinier Hernández <rhernandeza@facinf.uho.edu.cu>
2015-12-02 15:01:05 -0500

    comprobando que las contraseñas del formulario de crearse cuenta coincida

--- mediaProject/account/static/own/create_account.js
+++ mediaProject/account/static/own/create_account.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
+    pass = $("#password");
+    pass_check = $("#password-check");
+    sex = $("#sex");
+    console.log(sex.val());
 
+    $("#create").click(function (event) {
+        if(pass.val() != pass_check.val()){
+            alert("las contrasenas no coinciden");
+            event.preventDefault();
+        }
+    });
 });
